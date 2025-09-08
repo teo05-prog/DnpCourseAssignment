a6dbf85ac7a4c2948bf118967f7e6dd28c3c289b
Teo <teodora.cristiana.stoicescu@gmail.com>
2025-09-08 13:52:32 +0200

    Initial commit

---
+++ Entities/Comment.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities;
+
+public class Comment
+{
+    public string Body { get; set; }
+    public int PostId { get; set; }
+    public int UserId { get; set; }
+    public int CommentId { get; set; }
+}
---
+++ Entities/User.cs
@@ -0,0 +1,8 @@
+﻿namespace Entities;
+
+public class User
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public int UserId { get; set; }
+}
